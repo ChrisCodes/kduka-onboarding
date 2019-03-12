c86ae4c18f3e0eb5182f8bd3ec4039eb91fb2360
Chris <chrisnewcarson@gmail.com>
2019-03-13 00:34:35 +0300

    began work on desktop versions. changed navigation colors to KDuka's official green color:

--- includes/step_1.php
+++ includes/step_1.php
@@ -1,44 +1,88 @@
 
 <div class="main-body step-one">
 
-	<div class="container">
-	
-		<div class="clear-horizontal-nav"></div>
+	<div class="clear-horizontal-nav"></div>
 
 
-		
-		<h1 class="step-head">Create an Online Shop today.</h1>
+	<div class="container-fluid">
 
 
-		<form action="#" method="POST" class="add-shop-name">
-		
-		<!-- Input shop name -->
-		<div class="input-group">
+		<div class="row">
 
-			<span class="field-label">What's your shops name?</span>
+			<!-- form section -->
+			<div class="col-xs-12 col-sm-12 col-md-6">
+				
+				<div class="container-fluid">
 
-			<input type="text" name="shop-name" class="mat_input_field" id="shop-name" placeholder="e.g Becky's flowers" />
+					<div class="main-contentx">
+
+						<div class="clear-horizontal-nav"></div>
+						<div class="clear-horizontal-nav"></div>
+
+
+
+						<h1 class="step-head">Create an Online Shop today.</h1>
+
+
+						<form action="#" method="POST" class="add-shop-name">
+						
+						<!-- Input shop name -->
+						<div class="input-group">
+
+							<span class="field-label">What's your shops name?</span>
+
+							<input type="text" name="shop-name" class="mat_input_field" id="shop-name" placeholder="e.g Becky's flowers" />
+
+						</div>
+
+
+						<!-- Input button -->
+						<div class="input-group">
+							<!-- <input type="submit" class="button white-first" value="Create" /> -->
+
+							<a href="?step=2" class="button white-first">Create</a>
+							
+						</div>
+
+
+						</form>
+						
+					</div>
+					
+
+				</div>
+
+			</div>
+			<!-- form section -->
+
+
+
+
+
+
+			<!-- Image section -->
+			<div class="col-xs-12 col-sm-12 col-md-6 right-banner-image">
+
+
+				<div class="stat-slide-box">
+
+					<div class="stat-desc">
+						<p>Kenyans online from time to time.</p>
+
+						<small>Internet Statistics</small>
+					 </div>
+
+					<div class="stat-data">85%</div>
+
+				</div>
+				
+			</div>
+
+
 
 		</div>
-
-
-		<!-- Input button -->
-		<div class="input-group">
-			<!-- <input type="submit" class="button white-first" value="Create" /> -->
-
-			<a href="?step=2" class="button white-first">Create</a>
-			
-		</div>
-
-
-		</form>
-		
-
-
-		
-
-
 	</div>
+
 
 
 
